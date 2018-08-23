c545e406d2b83097cfc74b9a30f46907f0a9b070
pskrunner14 <pskrunner14@gmail.com>
2018-08-23 22:12:00 +0530

    fixes tl global vars init deprecated issue #13 and impove code readability

--- main_simple_seq2seq.py
+++ main_simple_seq2seq.py
@@ -6,19 +6,22 @@ References
 ----------
 http://suriyadeepan.github.io/2016-12-31-practical-seq2seq/
 """
-import tensorflow as tf
-import tensorlayer as tl
-from tensorlayer.layers import *
-
-import tensorflow as tf
-import numpy as np
 import time
 
-###============= prepare data
+import tensorflow as tf
+import tensorlayer as tl
+import numpy as np
+
+from tensorlayer.layers import DenseLayer, EmbeddingInputlayer, Seq2Seq, retrieve_seq_length_op2
+from sklearn.utils import shuffle
+
 from data.twitter import data
-metadata, idx_q, idx_a = data.load_data(PATH='data/twitter/')                   # Twitter
-# from data.cornell_corpus import data
-# metadata, idx_q, idx_a = data.load_data(PATH='data/cornell_corpus/')          # Cornell Moive
+from data.cornell_corpus import data
+
+# Data Preparation
+data_corpus = 'twitter'
+
+metadata, idx_q, idx_a = data.load_data(PATH='data/{}/'.format(data_corpus))
 (trainX, trainY), (testX, testY), (validX, validY) = data.split_dataset(idx_q, idx_a)
 
 trainX = trainX.tolist()
@@ -35,12 +38,14 @@ testY = tl.prepro.remove_pad_sequences(testY)
 validX = tl.prepro.remove_pad_sequences(validX)
 validY = tl.prepro.remove_pad_sequences(validY)
 
-###============= parameters
+# Parameters
 xseq_len = len(trainX)#.shape[-1]
 yseq_len = len(trainY)#.shape[-1]
+
 assert xseq_len == yseq_len
+
 batch_size = 32
-n_step = int(xseq_len/batch_size)
+n_step = xseq_len // batch_size
 xvocab_size = len(metadata['idx2w']) # 8002 (0~8001)
 emb_dim = 1024
 
@@ -67,34 +72,44 @@ target_mask : [1, 1, 1, 1, 0]
 """
 
 print("encode_seqs", [idx2w[id] for id in trainX[10]])
+
 target_seqs = tl.prepro.sequences_add_end_id([trainY[10]], end_id=end_id)[0]
-    # target_seqs = tl.prepro.remove_pad_sequences([target_seqs], pad_id=pad_id)[0]
 print("target_seqs", [idx2w[id] for id in target_seqs])
+
 decode_seqs = tl.prepro.sequences_add_start_id([trainY[10]], start_id=start_id, remove_last=False)[0]
-    # decode_seqs = tl.prepro.remove_pad_sequences([decode_seqs], pad_id=pad_id)[0]
 print("decode_seqs", [idx2w[id] for id in decode_seqs])
+
 target_mask = tl.prepro.sequences_get_mask([target_seqs])[0]
 print("target_mask", target_mask)
 print(len(target_seqs), len(decode_seqs), len(target_mask))
 
-###============= model
+"""
+Creates the Seq2Seq Model
+"""
+
 def model(encode_seqs, decode_seqs, is_train=True, reuse=False):
+    
     with tf.variable_scope("model", reuse=reuse):
+        
         # for chatbot, you can use the same embedding layer,
         # for translation, you may want to use 2 seperated embedding layers
         with tf.variable_scope("embedding") as vs:
+            
             net_encode = EmbeddingInputlayer(
                 inputs = encode_seqs,
                 vocabulary_size = xvocab_size,
                 embedding_size = emb_dim,
                 name = 'seq_embedding')
+
             vs.reuse_variables()
             tl.layers.set_name_reuse(True) # remove if TL version == 1.8.0+
+
             net_decode = EmbeddingInputlayer(
                 inputs = decode_seqs,
                 vocabulary_size = xvocab_size,
                 embedding_size = emb_dim,
                 name = 'seq_embedding')
+
         net_rnn = Seq2Seq(net_encode, net_decode,
                 cell_fn = tf.contrib.rnn.BasicLSTMCell,
                 n_hidden = emb_dim,
@@ -106,54 +121,53 @@ def model(encode_seqs, decode_seqs, is_train=True, reuse=False):
                 n_layer = 3,
                 return_seq_2d = True,
                 name = 'seq2seq')
+
         net_out = DenseLayer(net_rnn, n_units=xvocab_size, act=tf.identity, name='output')
+    
     return net_out, net_rnn
 
-# model for training
+# Training Model
 encode_seqs = tf.placeholder(dtype=tf.int64, shape=[batch_size, None], name="encode_seqs")
 decode_seqs = tf.placeholder(dtype=tf.int64, shape=[batch_size, None], name="decode_seqs")
 target_seqs = tf.placeholder(dtype=tf.int64, shape=[batch_size, None], name="target_seqs")
-target_mask = tf.placeholder(dtype=tf.int64, shape=[batch_size, None], name="target_mask") # tl.prepro.sequences_get_mask()
+target_mask = tf.placeholder(dtype=tf.int64, shape=[batch_size, None], name="target_mask") 
+
 net_out, _ = model(encode_seqs, decode_seqs, is_train=True, reuse=False)
 
-# model for inferencing
+# Inference Model
 encode_seqs2 = tf.placeholder(dtype=tf.int64, shape=[1, None], name="encode_seqs")
 decode_seqs2 = tf.placeholder(dtype=tf.int64, shape=[1, None], name="decode_seqs")
+
 net, net_rnn = model(encode_seqs2, decode_seqs2, is_train=False, reuse=True)
 y = tf.nn.softmax(net.outputs)
 
-# loss for training
-    # print(net_out.outputs)    # (?, 8004)
-    # print(target_seqs)    # (32, ?)
-    # loss_weights = tf.ones_like(target_seqs, dtype=tf.float32)
-    # loss = tf.contrib.legacy_seq2seq.sequence_loss(net_out.outputs, target_seqs, loss_weights, yvocab_size)
-loss = tl.cost.cross_entropy_seq_with_mask(logits=net_out.outputs, target_seqs=target_seqs, input_mask=target_mask, return_details=False, name='cost')
+# Loss Function
+loss = tl.cost.cross_entropy_seq_with_mask(logits=net_out.outputs, target_seqs=target_seqs, 
+    input_mask=target_mask, return_details=False, name='cost')
 
 net_out.print_params(False)
 
-lr = 0.0001
+# Optimizer
+lr = 0.001
 train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
-# Truncated Backpropagation for training (option)
-# max_grad_norm = 30
-# grads, _ = tf.clip_by_global_norm(tf.gradients(loss, net_out.all_params),max_grad_norm)
-# optimizer = tf.train.GradientDescentOptimizer(lr)
-# train_op = optimizer.apply_gradients(zip(grads, net_out.all_params))
 
-# sess = tf.InteractiveSession()
+# Init Session
 sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
-tl.layers.initialize_global_variables(sess)
+sess.run(tf.global_variables_initializer())
+
 tl.files.load_and_assign_npz(sess=sess, name='n.npz', network=net)
 
-###============= train
+# Start Training
 n_epoch = 50
 for epoch in range(n_epoch):
-    epoch_time = time.time()
-    ## shuffle training data
-    from sklearn.utils import shuffle
+
     trainX, trainY = shuffle(trainX, trainY, random_state=0)
-    ## train an epoch
+
     total_err, n_iter = 0, 0
+    epoch_time = time.time()
+
     for X, Y in tl.iterate.minibatches(inputs=trainX, targets=trainY, batch_size=batch_size, shuffle=False):
+
         step_time = time.time()
 
         X = tl.prepro.pad_sequences(X)
@@ -185,7 +199,7 @@ for epoch in range(n_epoch):
 
         total_err += err; n_iter += 1
 
-        ###============= inference
+        # Inference
         if n_iter % 1000 == 0:
             seeds = ["happy birthday have a nice day",
                     "donald trump won last nights presidential debate according to snap online polls"]
@@ -193,17 +207,17 @@ for epoch in range(n_epoch):
                 print("Query >", seed)
                 seed_id = [w2idx[w] for w in seed.split(" ")]
                 for _ in range(5):  # 1 Query --> 5 Reply
-                    # 1. encode, get state
+                    # Encode and get state
                     state = sess.run(net_rnn.final_state_encode,
                                     {encode_seqs2: [seed_id]})
-                    # 2. decode, feed start_id, get first word
-                    #   ref https://github.com/zsdonghao/tensorlayer/blob/master/example/tutorial_ptb_lstm_state_is_tuple.py
+                    # Decode, feed start_id and get first word
+                    # https://github.com/zsdonghao/tensorlayer/blob/master/example/tutorial_ptb_lstm_state_is_tuple.py
                     o, state = sess.run([y, net_rnn.final_state_decode],
                                     {net_rnn.initial_state_decode: state,
                                     decode_seqs2: [[start_id]]})
                     w_id = tl.nlp.sample_top(o[0], top_k=3)
                     w = idx2w[w_id]
-                    # 3. decode, feed state iteratively
+                    # Decode and feed state iteratively
                     sentence = [w]
                     for _ in range(30): # max sentence length
                         o, state = sess.run([y, net_rnn.final_state_decode],
