1a1980f1066bce1509558a454128e56bc9fcdd14
Prabhsimran Singh <pskrunner14@gmail.com>
2018-10-06 17:13:40 +0530

    remove exception handler

--- main.py
+++ main.py
@@ -142,11 +142,8 @@ def train(data_corpus, batch_size, num_epochs, learning_rate, inference_mode):
         print('--------------')
         while True:
             input_seq = input('Enter Query: ')
-            try:
-                sentence = inference(input_seq)
-                print(" >", ' '.join(sentence))
-            except KeyError:
-                print('Unknown token. Please try again!')
+            sentence = inference(input_seq)
+            print(" >", ' '.join(sentence))
     else:
         seeds = ["happy birthday have a nice day",
                  "donald trump won last nights presidential debate according to snap online polls"]
