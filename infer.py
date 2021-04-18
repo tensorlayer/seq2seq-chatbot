3245e6c88809066108ea636c2c20c323f2f10507
chuks <jodwin1420@gmail.com>
2021-04-18 08:28:42 +0100

    created top_n which varies the amount of replies

--- infer.py
+++ infer.py
@@ -7,13 +7,15 @@ import tensorlayer as tl
 load_weights = tl.files.load_npz(name='saved/model.npz')
 tl.files.assign_weights(load_weights, model_)
 
+top_n = 3
 
 def respond(input):
-    sentence = inference(input, 3)
+    sentence = inference(input, top_n)
     response=' '.join(sentence)
     return response
 
 while True:
     userInput = input("Query > ")
-    print("bot# ", respond(userInput))
+    for i in range(top_n):
+        print("bot# ", respond(userInput))
 
